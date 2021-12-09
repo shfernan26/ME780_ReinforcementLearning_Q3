624f5599232bcdc57fcf75b5dd52fc5b0a1eeabf
shfernan26 <shfernan@uwaterloo.ca>
2021-12-09 12:32:19 -0500

    sample code

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import numpy as np
+import pylab
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("darkgrid")
+import random
+
+gamma = 1 # discounting rate
+rewardSize = -1
+gridSize = 4
+terminationStates = [[0,0], [gridSize-1, gridSize-1]]
+actions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+numIterations = 1000
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def actionRewardFunction(initialPosition, action):
+    if initialPosition in terminationStates:
+        return initialPosition, 0
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    reward = rewardSize
+    finalPosition = np.array(initialPosition) + np.array(action)
+    if -1 in finalPosition or 4 in finalPosition:
+        finalPosition = initialPosition
+
+    return finalPosition, reward
+
+valueMap = np.zeros((gridSize, gridSize))
+states = [[i, j] for i in range(gridSize) for j in range(gridSize)]
+
+# values of the value function at step 0
+print("Init value map: ")
+print(valueMap)
+
+deltas = []
+for it in range(numIterations):
+    copyValueMap = np.copy(valueMap)
+    deltaState = []
+    for state in states:
+        weightedRewards = 0
+        for action in actions:
+            finalPosition, reward = actionRewardFunction(state, action)
+            weightedRewards += (1/len(actions))*(reward+(gamma*valueMap[finalPosition[0], finalPosition[1]]))
+        deltaState.append(np.abs(copyValueMap[state[0], state[1]]-weightedRewards))
+        copyValueMap[state[0], state[1]] = weightedRewards
+    deltas.append(deltaState)
+    valueMap = copyValueMap
+    if it in [0,1,2,9, 99, numIterations-1]:
+        print("Iteration {}".format(it+1))
+        print(valueMap)
+        print("")
+
+plt.figure(figsize=(20, 10))
+plt.plot(deltas)
+plt.show()
